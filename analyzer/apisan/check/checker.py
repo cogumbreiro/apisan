7312cbaacd583ceb939e2d6669bfedfa53f0e4bd
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-01-19 10:08:37 -0500

    Better UX for the bug report output.

--- analyzer/apisan/check/checker.py
+++ analyzer/apisan/check/checker.py
@@ -1,6 +1,74 @@
 #!/usr/bin/env python3
 from ..lib import config
 from ..lib.store import Store
+import os.path
+import re
+
+CONSTANTS = {
+    -128: "INT8_MIN",
+    128: "INT8_MAX",
+    255: "UINT8_MAX",
+    -32768: "INT16_MIN",
+    32767: "INT16_MAX",
+    65535: "UINT16_MAX",
+    -2147483648: "INT32_MIN",
+    2147483647: "INT32_MAX",
+    4294967295: "UINT32_MAX",
+    -9223372036854775808: "INT64_MIN",
+    9223372036854775807: "INT64_MAX",
+}
+
+def parse_constant_type(k):
+    left, _ = k.split("_")
+    return left
+
+def humanize_num(n):
+    return CONSTANTS.get(n, str(n))
+
+def humanize_ival(p):
+    if p[0] == p[1]:
+        return humanize_num(p[0])
+    else:
+        return "[%s,%s]" % (humanize_num(p[0]), humanize_num(p[1]))
+
+def humanize_range(r):
+    if len(r) == 1:
+        return "== " + humanize_ival(r[0])
+    if len(r) == 2:
+        ((lo1, hi1), (lo2, hi2)) = r
+        if hi1 + 2 == lo2 and lo1 in CONSTANTS and hi2 in CONSTANTS:
+            if parse_constant_type(CONSTANTS[lo1]) == parse_constant_type(CONSTANTS[hi2]):
+              return "!= " + str(hi1 + 1)
+            
+    return "in {" + ", ".join(humanize_ival(p) for p in r) + "}"
+
+def print_line(enc_filename):
+    try:
+        orig_fname, fname, lineno = enc_filename.split(":")
+        lineno = int(lineno)
+    except ValueError:
+        return
+    if not os.path.exists(orig_fname):
+        return
+    expr = re.compile('# ([0-9]+) "([^"]+)".*')
+    with open(orig_fname) as fp:
+        counter = 1
+        curr_line = 1
+        curr_fname = os.path.basename(orig_fname)
+        for line in fp:
+            res = expr.match(line)
+            if res is not None:
+                curr_fname = res.group(2)
+                curr_line = int(res.group(1))
+                counter += 1
+                continue
+
+            if curr_fname == fname and lineno == curr_line:
+                return f"{orig_fname}:{counter}: {line}"
+
+            curr_line += 1
+            counter += 1
+    return
 
 class BugReport:
     def __init__(self, score, code, key, ctx, references=None):
@@ -37,6 +105,14 @@ class BugReport:
                 self.score, self.code, self.key, self.ctx,
                 self.get_references(self.ctx.config.reference)
             )
+
+
+    def __str__(self):
+        refs = self.get_references(self.ctx.config.reference) if self.references is not None else ""
+        ctx = humanize_range(self.ctx)
+        line = print_line(self.code)
+        line = "\n" + line if line is not None else ""
+        return f"{self.score:.2%} {self.code} '{self.key}' {ctx} {refs}{line}"
 
 class Context:
     def __init__(self, config):
