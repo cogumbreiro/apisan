0522f9c64ed2b45ccf81c52986bb4c8e8fca4ca6
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-06-30 15:07:08 +0200

    Compile fewer features of LLVM.

--- setup.sh
+++ setup.sh
@@ -2,9 +2,20 @@
 LLVM_DIR=bin/llvm
 
 build_llvm() {
-  mkdir -p bin/llvm
-  pushd bin/llvm
-  cmake ../../llvm -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release
+  mkdir -p "$LLVM_DIR"
+  LLVM_SRC_DIR="$PWD/llvm"
+  pushd "$LLVM_DIR"
+  export CC=gcc-4.8
+  export CXX=g++-4.8
+  cmake "$LLVM_SRC_DIR" \
+    -DLLVM_TARGETS_TO_BUILD=X86 \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_INCLUDE_DOCS=0 \
+    -DLLVM_INCLUDE_TESTS=0 \
+    -DLLVM_INCLUDE_EXAMPLES=0 \
+    -DLLVM_INCLUDE_UTILS=0 \
+    -DLLVM_BINDINGS_LIST="" \
+    -DWITH_POLLY=0
   make -j$(nproc)
   popd
 }
