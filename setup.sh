621cc03c6049e839fa9591c54f95f6cc2e5f1802
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-08-07 16:17:17 -0400

    Fixed return value.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 LLVM_DIR=bin/llvm
-
-build_llvm() {
-  mkdir -p "$LLVM_DIR"
-  LLVM_SRC_DIR="$PWD/llvm"
-  pushd "$LLVM_DIR"
-  export CC=gcc-4.8
-  export CXX=g++-4.8
-  cmake "$LLVM_SRC_DIR" \
-    -DLLVM_TARGETS_TO_BUILD=X86 \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_INCLUDE_DOCS=0 \
-    -DLLVM_INCLUDE_TESTS=0 \
-    -DLLVM_INCLUDE_EXAMPLES=0 \
-    -DLLVM_INCLUDE_UTILS=0 \
-    -DLLVM_BINDINGS_LIST="" \
-    -DWITH_POLLY=0
-  make -j$(nproc)
-  popd
-}
-
-build_llvm
+mkdir -p "$LLVM_DIR"
+LLVM_SRC_DIR="$PWD/llvm"
+pushd "$LLVM_DIR"
+export CC=gcc-4.8
+export CXX=g++-4.8
+cmake "$LLVM_SRC_DIR" \
+  -DLLVM_TARGETS_TO_BUILD=X86 \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_INCLUDE_DOCS=0 \
+  -DLLVM_INCLUDE_TESTS=0 \
+  -DLLVM_INCLUDE_EXAMPLES=0 \
+  -DLLVM_INCLUDE_UTILS=0 \
+  -DLLVM_BINDINGS_LIST="" \
+  -DWITH_POLLY=0
+make -j$(nproc)
+ret=$?
+popd
+exit $ret
